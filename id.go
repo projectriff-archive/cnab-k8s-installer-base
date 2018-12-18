7c4568f720956da0857c396730d47e39b72d81bc
Glyn Normington <gnormington@pivotal.io>
2018-12-18 17:23:01 +0000

    Avoid colons in image filenames
    Colons in filenames are anathema to the Windows kernel.
    
    This commit produces PFS distribution archives which are incompatible with
    those of v0.1.0. The new `pfs image push` fails with an old archive and the
    old `pfs image push` fails with a new archive. That's ok because v0.1.0 was
    only an alpha release.
    
    Fixes https://github.com/pivotal-cf/pfs/issues/185

--- id.go
+++ id.go
@@ -1,6 +1,9 @@
 package image
 
-import "github.com/opencontainers/go-digest"
+import (
+	"github.com/opencontainers/go-digest"
+	"strings"
+)
 
 // Id is an image id, which happens to be represented as a digest string. An image id
 // is based on the binary contents of an image, but not with its name (see Digest).
@@ -20,4 +23,10 @@ func NewId(id string) Id {
 
 func (id Id) String() string {
 	return string(id.dig)
+}
+
+// Filename returns a filesystem-friendly name for this image id.
+// Filenames in Windows cannot contain ":".
+func (id Id) Filename() string {
+	return strings.Replace(id.String(), ":", "-", -1)
 }
