333ec39a4dc948c91a07ac09b470b3ba9a4478b1
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-02-11 08:44:41 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,35 @@
+module cnab-k8s-installer-base
+
+require (
+	github.com/ghodss/yaml v1.0.0
+	github.com/gogo/protobuf v1.2.0 // indirect
+	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/google/gofuzz v0.0.0-20170612174753-24818f796faf // indirect
+	github.com/googleapis/gnostic v0.2.0 // indirect
+	github.com/gregjones/httpcache v0.0.0-20190203031600-7a902570cb17 // indirect
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/jinzhu/copier v0.0.0-20180308034124-7e38e58719c3
+	github.com/json-iterator/go v1.1.5 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.7.0 // indirect
+	github.com/onsi/gomega v1.4.3 // indirect
+	github.com/peterbourgon/diskv v2.0.1+incompatible // indirect
+	github.com/projectriff/riff v0.2.0
+	github.com/spf13/pflag v1.0.3 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20190208162236-193df9c0f06f // indirect
+	golang.org/x/net v0.0.0-20190206173232-65e2d4e15006 // indirect
+	golang.org/x/oauth2 v0.0.0-20190130055435-99b60b757ec1 // indirect
+	golang.org/x/sys v0.0.0-20190209173611-3b5209105503 // indirect
+	golang.org/x/time v0.0.0-20181108054448-85acf8d2951c // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+	k8s.io/api v0.0.0-20190208171344-de494049e22a // indirect
+	k8s.io/apiextensions-apiserver v0.0.0-20190208173442-01b4f3c039db
+	k8s.io/apimachinery v0.0.0-20190210215030-4521e64aecd3
+	k8s.io/client-go v10.0.0+incompatible
+	k8s.io/klog v0.1.0 // indirect
+	sigs.k8s.io/structured-merge-diff v0.0.0-20190130003954-e5e029740eb8 // indirect
+	sigs.k8s.io/yaml v1.1.0 // indirect
+)
---
+++ pkg/crd/manifest.go
@@ -0,0 +1,158 @@
+/*
+ * Copyright 2019 The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package crd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/ghodss/yaml"
+	"github.com/jinzhu/copier"
+	"github.com/projectriff/riff/pkg/fileutils"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"net/url"
+	"path/filepath"
+	"strings"
+)
+
+type ResourceChecks struct {
+	Kind     string               `json:"kind,omitempty"`
+	Selector metav1.LabelSelector `json:"selector,omitempty"`
+	JsonPath string               `json:"jsonpath,omitempty"`
+	Pattern  string               `json:"pattern,omitempty"`
+}
+
+type Resource struct {
+	Path      string           `json:"path,omitempty"`
+	Content   string           `json:"content,omitempty"`
+	Name      string           `json:"name,omitempty"`
+	Namespace string           `json:"namespace,omitempty"`
+	Checks    []ResourceChecks `json:"checks,omitempty"`
+}
+
+type RiffResource struct {
+	System         []Resource `json:"system,omitempty"`
+	Initialization []Resource `json:"initialization,omitempty"`
+}
+
+type RiffSpec struct {
+	Images    []string     `json:"images,omitempty"`
+	Resources RiffResource `json:"resources,omitempty"`
+}
+
+type RiffStatus struct {
+	Status string `json:"status,omitempty"`
+}
+
+type Manifest struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec        RiffSpec   `json:"spec,omitempty"`
+	Status      RiffStatus `json:"status,omitempty"`
+}
+
+func (orig *Manifest) DeepCopyObject() runtime.Object {
+	result := &Manifest{}
+	copier.Copy(result, orig)
+	return result
+}
+
+func ResolveManifest(manifests map[string]*Manifest, path string) (*Manifest, error) {
+	if manifest, ok := manifests[path]; ok {
+		return manifest, nil
+	}
+	return NewManifest(path)
+}
+
+func NewManifest(path string) (*Manifest, error) {
+	var m Manifest
+	yamlFile, err := fileutils.Read(path, "")
+	if err != nil {
+		return nil, fmt.Errorf("Error reading manifest file: %v", err)
+	}
+
+	err = yaml.Unmarshal(yamlFile, &m)
+	if err != nil {
+		if strings.Contains(err.Error(), "did not find expected key") {
+			return nil, fmt.Errorf("Error parsing manifest file: %v. Please ensure that manifest has supported version:", err)
+		}
+		return nil, fmt.Errorf("Error parsing manifest file: %v", err)
+	}
+
+	supportedVersion := fmt.Sprintf("%s/%s", Group, Version)
+	if !strings.EqualFold(m.APIVersion, supportedVersion) {
+		return nil, errors.New(fmt.Sprintf("Unsupported version %s. Supported version is %s", m.APIVersion, supportedVersion))
+	}
+
+	err = checkCompleteness(m)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.VisitResources(checkResourcePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return &m, nil
+}
+
+func (m *Manifest) VisitResources(f func(res Resource) error) error {
+	var resources []Resource
+	resources = append(resources, m.Spec.Resources.System...)
+	resources = append(resources, m.Spec.Resources.Initialization...)
+	for _, resource := range resources {
+		err := f(resource)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func checkCompleteness(m Manifest) error {
+	var omission string
+	if m.Spec.Resources.System == nil {
+		omission = "system"
+	} else if m.Spec.Resources.Initialization == nil {
+		omission = "namespace-initialization"
+	} else {
+		return nil
+	}
+	return fmt.Errorf("manifest is incomplete: %s missing: %#v", omission, m)
+}
+
+func checkResourcePath(resource Resource) error {
+	if filepath.IsAbs(resource.Path) {
+		return fmt.Errorf("resources must use a http or https URL or a relative path: absolute path not supported: %s", resource)
+	}
+
+	u, err := url.Parse(resource.Path)
+	if err != nil {
+		return err
+	}
+	if u.Scheme == "http" || u.Scheme == "https" || (u.Scheme == "" && !filepath.IsAbs(u.Path)) {
+		return nil
+	}
+
+	if u.Scheme == "" {
+		return fmt.Errorf("resources must use a http or https URL or a relative path: absolute path not supported: %s", resource)
+	}
+
+	return fmt.Errorf("resources must use a http or https URL or a relative path: scheme %s not supported: %s", u.Scheme, resource)
+}
