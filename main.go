5f3df659d4e327d684f9d828f6938d9b59dcc054
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-03-27 23:48:29 -0700

    make env variables caps

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func install(path string) {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	err = knbClient.Relocate(manifest, os.Getenv("target_registry"))
+	err = knbClient.Relocate(manifest, os.Getenv("TARGET_REGISTRY"))
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -170,7 +170,7 @@ func setupLogging() {
 }
 
 func getLogLevel() log.Level {
-	requestedLevel := os.Getenv("log_level")
+	requestedLevel := os.Getenv("LOG_LEVEL")
 	if requestedLevel == "" {
 		return log.InfoLevel
 	}
