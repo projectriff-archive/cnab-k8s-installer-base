47d77133d796b0553ff415addd323a9854365ca0
Swapnil Bawaskar <sbawaskar@pivotal.io>
2019-05-17 07:25:25 -0700

    add diagnostics for ci

---
+++ ci/diagnostics.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl get deployments,services,pods --all-namespaces || true
+echo "FAILING PODS:"
+kubectl get pods --all-namespaces --field-selector=status.phase!=Running \
+| tail -n +2 | awk '{print "-n", $1, $2}' | xargs -L 1 kubectl describe pod || true
+echo "NODE:"
+kubectl describe node || true
